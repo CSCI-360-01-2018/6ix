de09c8de8c144f5873dc09604c4042c14009dd11
Megan Landau <landaumd@g.cofc.edu>
2018-03-25 16:44:33 -0400

    adding radio usage

--- code/src/com/csci360/alarmclock/Radio.java
+++ code/src/com/csci360/alarmclock/Radio.java
@@ -5,6 +5,11 @@
  */
 package com.csci360.alarmclock;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Radio {
 	public int amFrequency = 0; // 540-1600 kHZ for 106 possible bands
@@ -12,6 +17,41 @@ public class Radio {
 	public String currentMode = "AM";	
 	public int volume = 15; // range 0-100
 	public boolean radioOn = false;
+        List<Station> amStations = new ArrayList<Station>();
+        List<Station> fmStations = new ArrayList<Station>();
+        
+        public Radio(){
+            generateStations("am");
+            generateStations("fm");
+        }
+        
+        public List<Station> getStations(String amFm){
+            return amFm.equals("am") ? amStations : fmStations;
+        }
+        
+        public void generateStations(String amFm){
+            String filePath = System.getProperty("user.dir");
+            
+            if (amFm.equals("am")){
+                filePath += "/src/com/csci360/alarmclock/soundsAM";
+                System.out.println(filePath);
+                File music = new File(filePath);
+
+                File[] songs = music.listFiles();
+
+                for (File song : songs) {
+                    System.out.println(song.getPath());
+                    amStations.add(new Station(song));
+                }
+            } else {
+                filePath += "/src/com/csci360/alarmclock/soundsFM";
+                File music = new File(filePath);
+                File[] songs = music.listFiles();
+                for (File song : songs) {
+                    fmStations.add(new Station(song));
+                }
+            }
+        }
 	
 	public void setAmFmMode(String mode){
 		currentMode = mode;
@@ -22,11 +62,11 @@ public class Radio {
 	}
 	
 	public void setFrequency(String mode, int frequency){
-		if (mode.equals("AM")){
-			amFrequency = frequency;
-		} else {
-			fmFrequency = frequency;
-		}
+            if (mode.equals("AM")){
+                    amFrequency = frequency;
+            } else {
+                    fmFrequency = frequency;
+            }
 	}
 	
 	public String getCurrentMode(){
@@ -46,21 +86,36 @@ public class Radio {
 	}
 	
 	public void play(){
-		if (radioOn){
-			System.out.println("radio is playing");
-			// get frequency? get mode? play frequency and mode?
-		} else {
-			System.out.println("Radio is not on.");
-		}
+            if (radioOn){
+                    System.out.println("radio is playing");
+                    // get frequency? get mode? play frequency and mode?
+            } else {
+                    System.out.println("Radio is not on.");
+            }
 	}
 	
 	public static void main(String[] args){
-		Radio r = new Radio();
-		r.setFrequency("AM", 12);
-		r.toggleRadioOnOff();
-		r.setVolume(44);
-		System.out.println(r.getCurrentFrequency());
-		r.play();
+            
+            
+            Radio r = new Radio();
+
+            try {
+                for (Station stat : r.getStations("am")){
+                    stat.play();
+//                    stat.stop();
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            
+//		Radio r = new Radio();
+//		r.setFrequency("AM", 12);
+//		r.toggleRadioOnOff();
+//		r.setVolume(44);
+//		System.out.println(r.getCurrentFrequency());
+//		r.play();
 	}
 }
 
---
+++ code/src/com/csci360/alarmclock/Station.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.csci360.alarmclock;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Station {
+    int BUFFER_SIZE = 4096;
+    private SourceDataLine dataLine;
+    private AudioFormat format;
+    private AudioInputStream inputStream;
+
+    public Station(File audioFile){
+        try {
+            this.inputStream = AudioSystem.getAudioInputStream(audioFile);
+            this.format = inputStream.getFormat();
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+            this.dataLine = (SourceDataLine) AudioSystem.getLine(info);
+
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play() throws IOException {
+        try {
+            this.dataLine.open(this.format);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+        this.dataLine.start();
+        byte[] byteBuffer = new byte[BUFFER_SIZE];
+        int bytesRead;
+
+        while((bytesRead = this.inputStream.read(byteBuffer)) != -1){
+            this.dataLine.write(byteBuffer, 0, bytesRead);
+        }
+    }
+
+    public void stop() throws IOException {
+        this.dataLine.drain();
+        this.dataLine.stop();
+        this.dataLine.close();
+        this.inputStream.close();
+    }
+
+}
